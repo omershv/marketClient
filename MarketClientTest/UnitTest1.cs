01f7273f6d6b4824cd891d6e9be37ecf0916f3de
תומר גולומב <golombt@auth.ad.bgu.ac.il>
2017-03-05 11:58:47 +0200

    first few files for example

---
+++ MarketClientTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarketClient;
+using MarketClient.Utils;
+
+namespace MarketClientTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string url = "TEST";
+        private const string user = "TEST";
+        private const string privateKey = "TEST";
+
+        [TestMethod]
+        public void TestExample()
+        {
+            /// Attantion!, this code is not good practice! this was made for the sole purpose of being an example.
+            /// A real good code, should use defined classes and and not hardcoded values!
+            SimpleHTTPClient client = new SimpleHTTPClient();
+            var request = new{
+                type = "queryUser",
+            };
+            client.SendPostRequest(url,user,SimpleCtyptoLibrary.CreateToken(user,privateKey), request);
+        }
+    }
+}
